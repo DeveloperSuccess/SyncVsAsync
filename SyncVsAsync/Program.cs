df7cef6454207b12c94d720e99f5d44701d5c945
DeveloperSuccess <megafreez@gmail.com>
2023-06-24 17:03:56 +0500

    Промежуточные изменения

--- SyncVsAsync/Program.cs
+++ SyncVsAsync/Program.cs
@@ -8,21 +8,24 @@ foreach (var upperLimitOfRange in upperLimitsOfRanges)
 {
     var collection = Enumerable.Range(1, upperLimitOfRange);
 
-    var syncExecutionTime = await GetSyncExecutionTime(collection, millisecondsTimeout);
+    var syncExecutionTimeThread = await GetSyncExecutionTimeThread(collection, millisecondsTimeout);
+
+    var syncExecutionTimeDelay = await GetSyncExecutionTimeDelay(collection, millisecondsTimeout);
 
     var asyncExecutionTime = await GetAsyncExecutionTime(collection, millisecondsTimeout);
 
-    var difference = syncExecutionTime - asyncExecutionTime;
+    var asyncExecutionTimeInTasks = await GetAsyncExecutionTimeInTasks(collection, millisecondsTimeout);
 
-    Console.WriteLine($"Range limit: {upperLimitOfRange}; " +
-        $"Sync execution time: {syncExecutionTime} ms; " +
-        $"Async execution time: {asyncExecutionTime} ms; " +
-        $"Acceleration async execution : {difference} ms; ");
+    Console.WriteLine($"Iterations: {upperLimitOfRange}; " +
+        $"Sync thread: {syncExecutionTimeThread} ms; " +
+        $"Sync delay: {syncExecutionTimeDelay} ms; " +
+        $"Async: {asyncExecutionTime} ms; " +
+        $"Async in tasks: {asyncExecutionTimeInTasks} ms");
 }
 
 Console.Read();
 
-async Task<long> GetSyncExecutionTime(IEnumerable<int> collection, int millisecondsTimeout)
+async Task<long> GetSyncExecutionTimeThread(IEnumerable<int> collection, int millisecondsTimeout)
 {
     var executionTimeWithoutAsync = Stopwatch.StartNew();
 
@@ -33,11 +36,31 @@ async Task<long> GetSyncExecutionTime(IEnumerable<int> collection, int milliseco
     return executionTimeWithoutAsync.ElapsedMilliseconds;
 }
 
+async Task<long> GetSyncExecutionTimeDelay(IEnumerable<int> collection, int millisecondsTimeout)
+{
+    var executionTimeWithoutAsync = Stopwatch.StartNew();
+
+    await Task.WhenAll(collection.Select(_ => Task.Run(() => DelayAsync(millisecondsTimeout))));
+
+    executionTimeWithoutAsync.Stop();
+
+    return executionTimeWithoutAsync.ElapsedMilliseconds;
+}
+
 async Task<long> GetAsyncExecutionTime(IEnumerable<int> collection, int millisecondsTimeout)
 {
     var executionTimeWithAsync = Stopwatch.StartNew();
 
-    //await Task.WhenAll(collection.Select(async _ => await DelayAsync(millisecondsTimeout).ConfigureAwait(false)));
+    await Task.WhenAll(collection.Select(async _ => await DelayAsync(millisecondsTimeout).ConfigureAwait(false)));
+
+    executionTimeWithAsync.Stop();
+
+    return executionTimeWithAsync.ElapsedMilliseconds;
+}
+
+async Task<long> GetAsyncExecutionTimeInTasks(IEnumerable<int> collection, int millisecondsTimeout)
+{
+    var executionTimeWithAsync = Stopwatch.StartNew();
 
     await Task.WhenAll(collection.Select(_ => Task.Run(async () => await DelayAsync(millisecondsTimeout).ConfigureAwait(false))));
 
