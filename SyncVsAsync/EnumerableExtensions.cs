90503e298364c81c331afcf30967dab3ff1e87cc
DeveloperSuccess <megafreez@gmail.com>
2023-06-27 10:44:21 +0500

    Потестить расширение

---
+++ SyncVsAsync/EnumerableExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace SyncVsAsync
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<IEnumerable<T>> Partition<T>(this IEnumerable<T> source, int chunkSize)
+        {
+            while (source.Any())
+            {
+                yield return source.Take(chunkSize).ToList();
+                source = source.Skip(chunkSize).ToList();
+            }
+        }
+
+        public static async Task<IEnumerable<T>> WhenAllAsync<T>(this IEnumerable<Task<T>> source, int concurrently = 0, bool inNewThreads = false)
+        {
+            if (concurrently == 0)
+            {
+                if (inNewThreads == false)
+                    return await Task.WhenAll(source);
+
+                return await Task.WhenAll(source.Select(_ => Task.Run(() => _)));
+            }
+        }
+    }
+}
